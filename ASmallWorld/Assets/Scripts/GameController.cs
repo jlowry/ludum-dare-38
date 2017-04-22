b5136155faa0e9447a264a226c604f85b6889f5b
James Lowry <jlowry@users.noreply.github.com>
2017-04-22 17:11:25 +0100

    Basic Win/Lose Screen, Main menu, restartable

--- ASmallWorld/Assets/Scripts/GameController.cs
+++ ASmallWorld/Assets/Scripts/GameController.cs
@@ -1,39 +1,72 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace ASmallWorld {
     public class GameController : MonoBehaviour {
+        private enum GameState {
+            LOADING,
+            MAIN_MENU,
+            PLAYING,
+            END
+        }
+
+
+        private GameState state = GameState.LOADING;
+
         private Timer timer;
         private Player player;
         private OpinionPoller opinionPoller;
+        private MenuUI menuUI;
 
         void Awake() {
-            DontDestroyOnLoad(transform.gameObject);
+            //DontDestroyOnLoad(transform.gameObject);
         }
 
         // Use this for initialization
         void Start() {
+            WireComponents();
+            MainMenu();
+        }
+
+        public void ResetGame() {
+            state = GameState.LOADING;
+            SceneManager.LoadSceneAsync("Main", LoadSceneMode.Single);
+        }
+
+        private void WireComponents() {
+            menuUI = GameObject.FindGameObjectWithTag("MenuUI").GetComponent<MenuUI>();
             timer = GameObject.FindGameObjectWithTag("Timer").GetComponent<Timer>();
             player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
             opinionPoller = GameObject.FindGameObjectWithTag("OpinionPoller").GetComponent<OpinionPoller>();
             timer.TimesUp += TimesUp;
-            StartGame();
         }
 
+        public void MainMenu() {
+            menuUI.MainMenu();
+            state = GameState.MAIN_MENU;
+        }
 
-        void StartGame() {
+        public void StartGame() {
+            menuUI.GamePlaying();
+            state = GameState.PLAYING;
             timer.Reset();
             timer.StartTimer();
         }
 
         void TimesUp() {
             if (player.faction == opinionPoller.GetWinningFaction()) {
-                Debug.Log("Win");
+                menuUI.Victory();
             } else {
-                Debug.Log("Lose");
+                menuUI.Defeat();
             }
+            state = GameState.END;
+            
         }
+
+
+
     }
 }
 
---
+++ ASmallWorld/Assets/Scripts/MenuUI.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ASmallWorld {
+    public class MenuUI : MonoBehaviour {
+        public GameObject victory;
+        public GameObject defeat;
+        public GameObject mainMenu;
+
+        // Use this for initialization
+        void Awake() { }
+
+
+        public void Victory() {
+            mainMenu.SetActive(false);
+            victory.SetActive(true);
+            defeat.SetActive(false);
+        }
+
+        public void Defeat() {
+            mainMenu.SetActive(false);
+            victory.SetActive(false);
+            defeat.SetActive(true);
+        }
+
+        public void MainMenu() {
+            mainMenu.SetActive(true);
+            victory.SetActive(false);
+            defeat.SetActive(false);
+        }
+
+        public void GamePlaying() {
+            mainMenu.SetActive(false);
+            victory.SetActive(false);
+            defeat.SetActive(false);
+        }
+    }
+}
+
+
