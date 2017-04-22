19fccbfc3e5cf519dae6925cba727d4b39ef7b95
James Lowry <jlowry@users.noreply.github.com>
2017-04-22 09:44:29 +0100

    Player movement

---
+++ ASmallWorld/Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace ASmallWorld {
+    public class Player : MonoBehaviour {
+        public float speed = 10.0F;
+        public float gravity = 20.0F;
+        private CharacterController controller;
+
+        private Vector3 moveDirection = Vector3.zero;
+
+        // Use this for initialization
+        void Awake() {
+            controller = GetComponent<CharacterController>();
+        }
+
+        // Update is called once per frame
+        void Update() {
+            if (controller.isGrounded) {
+                moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+                moveDirection = transform.TransformDirection(moveDirection);
+                moveDirection *= speed;
+            }
+            moveDirection.y -= gravity * Time.deltaTime;
+            controller.Move(moveDirection * Time.deltaTime);
+        }
+    }
+}
+
+
