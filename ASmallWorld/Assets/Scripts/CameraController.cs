b8e16aa47c95dbc55c34c7b10ef26be6985c5ca7
James Lowry <jlowry@users.noreply.github.com>
2017-04-22 09:56:17 +0100

    Camera follow

---
+++ ASmallWorld/Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ASmallWorld {
+    public class CameraController : MonoBehaviour {
+        private Vector3 offset;
+        private Transform player;
+
+        // Use this for initialization
+        void Awake() {
+            // Get the players transform
+            player = GameObject.FindGameObjectWithTag("Player").transform;
+        }
+
+        void Start() {
+            offset = transform.position - player.position;
+        }
+
+        // LateUpdate is called once per frame
+        void LateUpdate() {
+            transform.position = player.transform.position + offset;
+        }
+    }
+}
