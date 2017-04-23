99db6dff75333329e66bc98b6c95fb93191a0c44
James Lowry <jlowry@users.noreply.github.com>
2017-04-23 21:55:14 +0100

    Textured rigged, and animated foxperson

--- ASmallWorld/Assets/Scripts/Player.cs
+++ ASmallWorld/Assets/Scripts/Player.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 namespace ASmallWorld {
     public class Player : MonoBehaviour {
-        public float speed = 10.0F;
+        public float speed = 4.0F;
         public float gravity = 20.0F;
         private CharacterController controller;
 
@@ -20,13 +20,22 @@ namespace ASmallWorld {
 
         // Update is called once per frame
         void Update() {
-            if (controller.isGrounded) {
+            if (controller.isGrounded)
+            {
+
                 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+                var m_TurnAmount = Mathf.Atan2(moveDirection.x, moveDirection.z);
+                // help the character turn faster (this is in addition to root rotation in the animation)
+                //float turnSpeed = Mathf.Lerp(m_StationaryTurnSpeed, m_MovingTurnSpeed, m_ForwardAmount);
+                transform.Rotate(0, m_TurnAmount * 100.0F * Time.deltaTime, 0);
                 moveDirection = transform.TransformDirection(moveDirection);
                 moveDirection *= speed;
             }
+
+
+
             moveDirection.y -= gravity * Time.deltaTime;
-            controller.Move(moveDirection * Time.deltaTime);
+            controller.Move(moveDirection * Time.deltaTime);           
         }
     }
 }
